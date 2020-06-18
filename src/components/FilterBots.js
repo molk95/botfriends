bb3c2c56fd5184c73776344b1bf68bd4b25f441d
molk95 <molk.saouabi@gmail.com>
2020-06-18 12:40:59 +0200

    search check

--- src/components/FilterBots.js
+++ src/components/FilterBots.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const FilterBots = ({filterFriends,searchbox}) => {
     return (
         <div className='searchToolBox' >
-           <input className='searchTool' placeholder='search...'  onChange={filterFriends} /> 
+           <input className='searchTool' placeholder='looking for robot friends..?'  onChange={filterFriends} /> 
         </div>
     )
 }
