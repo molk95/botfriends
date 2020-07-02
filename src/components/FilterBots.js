28e46ad9b5b980397b60dd749bbece55f666deaf
molk95 <molk.saouabi@gmail.com>
2020-07-02 16:02:03 +0200

    label

--- src/components/FilterBots.js
+++ src/components/FilterBots.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const FilterBots = ({searchChange,searchfield}) => {
     return (
-        <label className='searchToolBox' >
+        <label for='filter' className='searchToolBox' >
            <input type="search" className='searchTool' placeholder='looking for robot friends..?'  onChange={searchChange} /> 
         </label>
     )
