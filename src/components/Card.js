ee254cd977846ec1a3c6a96475e047002b697b60
molk95 <molk.saouabi@gmail.com>
2020-07-02 13:10:23 +0200

    po

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export const Card = ({id,name,email}) => {
+export const Card = ({id,name,email,po}) => {
     return (
         <div className='card'>
             <div>
