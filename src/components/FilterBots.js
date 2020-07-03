9130fd11a04d6a57023fd0645ab9adbfa09a1076
molk95 <molk.saouabi@gmail.com>
2020-07-03 11:14:15 +0200

    audit

--- src/components/FilterBots.js
+++ src/components/FilterBots.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const FilterBots = ({searchChange,searchfield}) => {
     return (
-        <label for='filter' className='pa2' >
+        <div className='pa2' >
+            <label for='filter' className="filterBots" >Search</label>
            <input id="filter" type="search" className='pa3 ba b--green bg-lightest-blue' placeholder='looking for robot friends..?'  onChange={searchChange} /> 
-        </label>
+        </div>
     )
 }
