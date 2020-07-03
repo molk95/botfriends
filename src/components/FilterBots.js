856a8dd6b9115cdb6eabaa9ac75493f11f7c9cbd
molk95 <molk.saouabi@gmail.com>
2020-07-03 09:41:05 +0200

    id

--- src/components/FilterBots.js
+++ src/components/FilterBots.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const FilterBots = ({searchChange,searchfield}) => {
     return (
         <label for='filter' className='pa2' >
-           <input type="search" className='pa3 ba b--green bg-lightest-blue' placeholder='looking for robot friends..?'  onChange={searchChange} /> 
+           <input id="filter" type="search" className='pa3 ba b--green bg-lightest-blue' placeholder='looking for robot friends..?'  onChange={searchChange} /> 
         </label>
     )
 }
