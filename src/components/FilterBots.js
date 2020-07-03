82c3b4bb82ea778ed55f415f4cf43921defc0dff
molk95 <molk.saouabi@gmail.com>
2020-07-03 11:26:58 +0200

    tachyons

--- src/components/FilterBots.js
+++ src/components/FilterBots.js
@@ -4,7 +4,7 @@ export const FilterBots = ({searchChange,searchfield}) => {
     return (
         <div className='pa2' >
             <label for='filter' className="filterBots" >Search</label>
-           <input id="filter" type="search" className='pa3 ba b--green bg-lightest-blue' placeholder='looking for robot friends..?'  onChange={searchChange} /> 
+           <input id="filter" type="search" className='pa3 ba b--green bg-lightest-blue' placeholder='looking for friends..?'  onChange={searchChange} /> 
         </div>
     )
 }
